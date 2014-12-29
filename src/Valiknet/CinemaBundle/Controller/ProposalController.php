fe0bda81ed6f2aa0e6d9b9d75a529823124899eb
Valik Grinevich <valik.v1per@gmail.com>
2014-12-29 21:55:45 +0200

    add new routing proposal and add list proposal page

---
+++ src/Valiknet/CinemaBundle/Controller/ProposalController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Valiknet\CinemaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
+
+class ProposalController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function listAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Template()
+     */
+    public function movieAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Template()
+     */
+    public function actorAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Template()
+     */
+    public function directorAction()
+    {
+        return [];
+    }
+}
